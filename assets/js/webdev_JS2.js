2835a4ad16bf47de7e6963269d74bf3e0e842a4c
nmab95 <nmbarce95@gmail.com>
2023-10-17 22:55:12 +0800

    Push change with adding comments

--- assets/js/webdev_JS2.js
+++ assets/js/webdev_JS2.js
@@ -1,6 +1,8 @@
+//Enter text
 var user = prompt('Please enter text: ');
 console.log(user);
 
+//Enter numbers to compute
 var x = prompt('Enter 1st number: ');
 var y = prompt('Enter 2nd number: ');
 var z = prompt('Enter 3rd number: ');
@@ -10,6 +12,7 @@ var product = n1*n2*n3;
 console.log('The sum of the numbers is: ', sum);
 console.log('The product of the numbers is: ', product);
 
+//Football team points
 var wins = prompt('Enter wins of your football team: ');
 var losses = prompt('Enter losses of your football team: ');
 var draws = prompt('Enter draws of your football team: ');
